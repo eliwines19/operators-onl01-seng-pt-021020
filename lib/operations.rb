2dd7f2ec0aba02d841ca174949859b8ff762fe3a
Elijah wines <elijahwines84@yahoo.com>
2020-02-16 16:49:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
 def unsafe?(speed)
+	if speed > 60
+		return true
 
+	elsif speed < 40
+		return true
+
+	elsif speed == 50
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    return true
+
+  elsif speed < 40
+    return true
+
+  elsif speed == 50
+    return false
+
+
 end
-	
-
-
